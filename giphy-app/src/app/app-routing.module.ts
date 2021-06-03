2a69cf218252cbf1b9c8b4ecb954fc4a6935e481
Iramar Vasquez <iramarvasquez@gmail.com>
2021-06-02 21:01:39 -0300

    Specifying the routes

--- giphy-app/src/app/app-routing.module.ts
+++ giphy-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GifsComponent } from './gifs/gifs.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+	{ path: 'Busqueda/:q', component: GifsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
